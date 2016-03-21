dbc4b540039ce6c4584d8434f20564f5eafb2305
Alex Lindsay <alexjlindsay@gmail.com>
2016-03-21 19:26:36 +0000

    Add form and submit option

--- app/actions.rb
+++ app/actions.rb
@@ -7,3 +7,18 @@ get '/songs' do
   @songs = Song.all
   erb :'songs/index'
 end
+
+get '/songs/new' do
+  erb :'songs/new'
+end
+
+post '/songs' do
+  @song = Song.new(
+    song_title: params[:song_title],
+    artist: params[:artist],
+    author: params[:author],
+    url: params[:url]
+  )
+  @song.save
+  redirect '/songs'
+end
